0a12c12a82ee095e62eb654d93129bcb2f97939b
zsimmons <zsimmons@verisys.com>
2019-01-07 12:52:24 -0700

    /blog built

--- DjangoBlog/blog/models.py
+++ DjangoBlog/blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+class PublishedManager(models.Manager):
+ 	def get_queryset(self):
+ 		return super(PublishedManager,
+ 					self).get_queryset()\
+ 						 .filter(status='published')
 class Post(models.Model):
  STATUS_CHOICES = (
  ('draft', 'Draft'),
@@ -23,3 +29,11 @@ class Post(models.Model):
  	ordering = ('-publish',)
  def __str__(self):
  	return self.title
+ objects = models.Manager()
+ published = PublishedManager()
+ def get_absolute_url(self):
+ 	 return reverse('blog:post_detail',
+ 					args=[self.publish.year,
+ 						  self.publish.month,
+ 						  self.publish.day,
+ 						  self.slug])
